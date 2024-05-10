70f8ed7206f49fd0bf3b300e473c6829fe7034d6
Christopher Forti <christopher.forti.79@gmail.com>
2024-05-10 19:11:41 -0400

    Played around with generative art, but had trouble typing it.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 // import styles from "./App.module.css";
 import { AddTodo } from "./components/AddTodo";
 import { Todos } from "./components/Todos";
+import Iframe from "react-iframe";
 
 function App() {
   const [todos, setTodos] = useState<string[]>([]);
@@ -11,6 +12,15 @@ function App() {
       <h1>Todo list</h1>
       <AddTodo todos={todos} setTodos={setTodos} />
       <Todos todos={todos} />
+      <Iframe
+        url="https://shaderpark.com/embed/-Np7TJ7lhufKxAsiV0ik"
+        width="640px"
+        height="320px"
+        id=""
+        className=""
+        display="block"
+        position="relative"
+      />
     </>
   );
 }
