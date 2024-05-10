b14d90928d20b3747a97947b5e7b9f4f3f7ec9d2
Christopher Forti <christopher.forti.79@gmail.com>
2024-05-10 19:12:39 -0400

    Added generative art to todo list, but had trouble with the type aspect.

---
+++ src/components/Iframe.tsx
@@ -0,0 +1,15 @@
+type urlProps = { style: URL[] };
+
+export function Iframe() {
+  return (
+    <Iframe
+      url="https://shaderpark.com/embed/-Np7TJ7lhufKxAsiV0ik"
+      width="640px"
+      height="320px"
+      id=""
+      className=""
+      display="block"
+      position="relative"
+    />
+  );
+}
