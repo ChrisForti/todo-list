ab8bbf7bce360f36ca893db9401aacb2af85169e
Christopher Forti <christopher.forti.79@gmail.com>
2024-09-30 20:50:01 -0400

    More styling changes for better mobil optimization.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import styles from "./Header.module.css";
 
 export function Header() {
-  return <header className={styles.body}>Todo list</header>;
+  return <header className={styles.body}>Todo's</header>;
 }
