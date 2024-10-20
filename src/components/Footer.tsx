d005a7293230a3e05578b2e51ce10a4e9dd84e24
Christopher Forti <christopher.forti.79@gmail.com>
2024-10-19 20:11:36 -0400

    Changed width on my image back to 50%.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 export function Footer() {
   return (
     <div className={styles.footer}>
-      <img src={MyImage} width="60%" height="50%" />
+      <img src={MyImage} width="50%" height="50%" />
     </div>
   );
 }
